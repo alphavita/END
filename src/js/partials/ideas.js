8ff75fcb03f2091e6668e3b490609e6fc0bcb8c1
Vita Alpha <alpha-2020@yandex.ru>
2016-10-21 20:51:55 +0300

    w

--- src/js/partials/ideas.js
+++ src/js/partials/ideas.js
@@ -1,9 +1,9 @@
-$(function(){
+;$(function(){
     $.support.cors = true;
     var gridItem = $('.grid-item');
     var searchForm = $('.search-area');
     var searchButton = $('.search-button');
-    var defaultQuery=['sea', 'fox', 'london','Relaxation', 'Travelling', 'africa', 'footbal', 'biathlon', 'fashion', 'circus', 'mobile', 'dog', 'Hollywood', 'flower' ];
+    var defaultQuery=['sea', 'fox', 'london','Relaxation', 'Travelling', 'africa', 'footbal', 'biathlon', 'fashion', 'Asia', 'mobile', 'dog', 'Hollywood', 'flower' ];
      // var defaultQuery=['00', '1', '2','3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13' ];
 	var rand=Math.floor(Math.random()*defaultQuery.length);
     var api = 'https://pixabay.com/api/?key=3225875-78b247f92a47a24f14259a8bd&q='+ defaultQuery[rand] +'&image_type=photo&min_width=300&min_height=200&per_page=7';
